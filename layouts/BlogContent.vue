03b365858846f699c78e08774dd73e9ee9c133a1
songxin_ye <13760612708@163.com>
2019-06-26 15:20:19 +0800

    使用 vssue

--- layouts/BlogContent.vue
+++ layouts/BlogContent.vue
@@ -10,16 +10,44 @@
         </div>
         <div style="clear: both"></div>
       </footer>
-      <Vssue :title="$title" />
+      <ClientOnly>
+        <VssueComponent :title="$title" :options="VssueOptions"/>
+      </ClientOnly>
     </div>
   </div>
 </template>
 
 <script>
 import Outline from "../components/Outline.vue";
+import { VssueComponent } from "vssue";
 export default {
   components: {
-    Outline
+    Outline,
+    VssueComponent
+  },
+  data() {
+    return {
+      VssueOptions: {
+        api: null,
+        owner: "OWNER_OF_REPO",
+        repo: "NAME_OF_REPO",
+        clientId: "YOUR_CLIENT_ID",
+        clientSecret: "YOUR_CLIENT_SECRET"
+      }
+    };
+  },
+  created() {
+    const comment = this.$themeConfig.comment;
+    if (!comment) return;
+    if (comment.platform === "gitee") {
+      this.VssueOptions.api = require("@vssue/api-gitee-v5");
+    } else if (comment.platform === "github") {
+      this.VssueOptions.api = require("@vssue/api-github-v3");
+    }
+    this.VssueOptions.owner = comment.owner;
+    this.VssueOptions.repo = comment.repo;
+    this.VssueOptions.clientId = comment.clientId;
+    this.VssueOptions.clientSecret = comment.clientSecret;
   }
 };
 </script>
