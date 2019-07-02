0d147674506c59b27cb33f65c0863720e42025c3
songxin_ye <13760612708@163.com>
2019-07-02 16:28:59 +0800

    增加 版权说明，增加 tag

---
+++ components/Statement.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="Statement" v-if="statement">
+    <p>
+      <span class="Statement-title">文章作者：</span>
+      <span class="Statement-content">{{statement.author}}</span>
+    </p>
+    <p class="singleText">
+      <span class="Statement-title">文章链接：</span>
+      <span class="Statement-content">
+        <a :href="path" target="_blank" >{{path}}</a>
+      </span>
+    </p>
+    <p>
+      <span class="Statement-title">版权声明：</span>
+      <span class="Statement-content">
+        本博客所有文章除特别声明外，均采用《
+        <a :href="statement.copyrightUrl" target="_blank" >{{statement.copyright}}</a>
+        》许可协议。转载请注明出处！
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    statement(){
+      return this.$themeConfig.statement
+    },
+    path(){
+      /** @type {string} */
+      const serveUrl = this.statement.serveUrl;
+      /** @type {string} */
+      const base = this.$site.base;
+      /** @type {string} */
+      const pagePath = this.$page.path;
+      let path = serveUrl;
+      if(serveUrl.lastIndexOf("/") === serveUrl.lengt - 1){
+        path += base.substring(1)
+      }else{
+        path += base
+      }
+      path += this.$page.path.substring(1)
+      return path
+    }
+  }
+}
+</script>
+
+
+<style lang="stylus">
+@import url("../styles/config");
+
+#Statement
+  background-color $borderColor
+  border-left 5px solid $accentColor
+  padding 20px 10px
+  margin 30px 0
+  .Statement-title
+    color #000
+  .Statement-content
+    font-size 0.9rem
+    color #666
+</style>
--- layouts/BlogContent.vue
+++ layouts/BlogContent.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="BlogContent" :class="{'hasHeaders':$page.headers}">
     <div class="theme-blog-content">
+      <div id="last-updated" v-if="$page.lastUpdated">
+        <span class="prefix">{{$themeConfig.lastUpdated || "最后更新："}}</span>
+        <span class="time">{{$page.lastUpdated}}</span>
+      </div>
       <Content />
-      <footer id="BlogContent-footer">
-        <div id="last-updated" v-if="$page.lastUpdated">
-          <span class="prefix">{{$themeConfig.lastUpdated || "最后更新："}}</span>
-          <span class="time">{{$page.lastUpdated}}</span>
-        </div>
-        <div style="clear: both"></div>
-      </footer>
+      <Statement />
+      <div id="BlogContent-tags">
+        <router-link
+          class="BlogContent-tag"
+          v-for="tag in $frontmatter.tags"
+          :key="tag"
+          :to="$tag._metaMap[tag].path"
+        ># {{tag}}</router-link>
+      </div>
       <Vssue
         v-if="$themeConfig.comment && $frontmatter.comment !== false"
         :issueId="$frontmatter.issueId"
@@ -20,9 +26,11 @@
 
 <script>
 import Outline from "../components/Outline.vue";
+import Statement from "../components/Statement.vue";
 export default {
   components: {
-    Outline
+    Outline,
+    Statement
   }
 };
 </script>
@@ -32,14 +40,17 @@ export default {
 
 #BlogContent.hasHeaders
   padding-right $sidebarWidth * 0.8
-#BlogContent-footer
-  padding-top 50px
-  padding-bottom 60px
 #last-updated
-  // float right
+  padding-top 50px
   font-size 0.9em
   .time
     color #aaa
+#BlogContent-tags
+  display flex
+  justify-content center
+  a.BlogContent-tag
+    margin 5px 10px
+    font-size 0.8rem
 #BlogContent
   a.vssue-header-comments-count
     cursor default
